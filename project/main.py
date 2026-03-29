5b83a60559aadae71ed299bc9b5bc8993fca282d
r1dji <101229429+r1dji@users.noreply.github.com>
2026-03-29 20:08:01 +0200

    lint errors resolved

--- project/main.py
+++ project/main.py
@@ -2,9 +2,5 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run('server:app', reload=True, host='0.0.0.0', port=8000)
-    try:
-        print("promena")
-    except:
-        print("error")
     #promena 1
     #promena posle dodatih testova
